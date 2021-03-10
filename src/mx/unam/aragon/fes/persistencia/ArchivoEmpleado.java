db95643e305222eacc352106f0f34f6d325a2fed
W4LDEMAR <sw_sosa@yahoo.com.mx>
2021-03-10 17:19:20 -0600

    Cuestionario 7. Clase 10-mar-21

--- src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
+++ src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
@@ -5,6 +5,10 @@
  */
 package mx.unam.aragon.fes.persistencia;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import mx.unam.aragon.fes.Empleado;
 
@@ -32,14 +36,33 @@ public class ArchivoEmpleado {
     
     public void guardarEmpleado(ArrayList<Empleado> datos ){
         //Codigo para grabar en disco duro
+        try {
+            ObjectOutputStream fSalida = new ObjectOutputStream(
+                    new FileOutputStream(archivo));
+            
+            fSalida.writeObject(datos);
+            fSalida.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     public ArrayList<Empleado> leerEmpleado(){
         ArrayList<Empleado> tmp = null;
-        
-        //leer disco duro
-        //subir arratlist
-        
+        try{
+            ObjectInputStream fLectura = new ObjectInputStream(
+                    new FileInputStream(archivo));
+            
+            tmp = (ArrayList<Empleado>) fLectura.readObject();
+            if (tmp == null) {
+                System.out.println("No hay nada");
+            } else {
+                System.out.println("Nombre" + tmp.size());
+            }
+            fLectura.close();
+        } catch (Exception ex) {
+            System.out.println("Error" + ex.toString());
+        }
         return tmp;
     }
 }
